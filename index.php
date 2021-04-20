3c8b05035ada9d4259d38f11dd376bab41bc65e4
unknown <titoyanac@gmail.com>
2021-04-19 21:27:39 -0500

    make ite beeter

--- index.php
+++ index.php
@@ -5,7 +5,42 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login con Facebook</title>
 </head>
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '184079043531405',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v10.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+</script>
 <body>
 	<h1>testing login con fb</h1>
+
+<fb:login-button 
+  scope="public_profile,email"
+  onlogin="checkLoginState();">
+</fb:login-button>
 </body>
 </html>
